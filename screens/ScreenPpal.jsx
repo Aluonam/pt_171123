d4e43061f291f6484e9b1bad53e3565c4e9f6cbe
Paula Cano <paulacanoroman@gmail.com>
2023-11-17 12:10:25 +0100

    consumoAPI que saca nombre

--- screens/ScreenPpal.jsx
+++ screens/ScreenPpal.jsx
@@ -2,23 +2,34 @@ import React, { useState, useEffect } from 'react'
 
 const ScreenPpal = () => {
 
-    const [dataAPI, setDataAPI] = useState()
+    const [dataAPI, setDataAPI] = useState([])
 
     const llamadaAPI = async () =>  {
         try{
             const llamada = await fetch('https://randomuser.me/api/')
             const data = await llamada.json()
-            console.log("datos",data.results)
+            console.log("datos API",data.results)
             setDataAPI(data.results)
+
         }
         catch(error){
             console.log("error detectado", error);
         }
     }
 
+    const userData = dataAPI.map((element)=>{
+        return(
+            <>
+            <h4>{element.name.first}</h4>
+            </>
+        )
+    })
+
   return (
     <div>
         <button onClick={()=>{llamadaAPI()}}>Pulsa</button>
+
+        {userData}
     </div>
   )
 }
