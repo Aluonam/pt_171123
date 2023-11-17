abdaad3d472be832b15d751597ba1a0ac1c15035
Paula Cano <paulacanoroman@gmail.com>
2023-11-17 13:42:41 +0100

    return new useState

--- components/ChangeLastLetter.jsx
+++ components/ChangeLastLetter.jsx
@@ -3,24 +3,28 @@ import React, { useState } from 'react'
 const ChangeLastLetter = () => {
 
     const [inputUser, setInputUser] = useState()
+    const [newSentence, setNewSentence] = useState()
 
     const changeLetter = ()=>{
-        const words = structuredClone(inputUser.split(" "))
+        const copyArr = structuredClone(inputUser)
+        const words = copyArr.split(" ")
         const change = words.map((oneWord)=>{
 
             const letterByLetter = oneWord.split("")
-            const upperLetters = letterByLetter.at(-1).toUpperCase()
-            letterByLetter[letterByLetter.length-1] = upperLetters
+            const lastLetters = letterByLetter.at(-1).toUpperCase()
+            letterByLetter[letterByLetter.length-1] = lastLetters
             return(
                 letterByLetter.join("")
             )
         })
-        console.log(change)
+        return(
+            setNewSentence(change.join(" "))
+        )
     
     }
   return (
     <>
-    {inputUser}
+    {newSentence}
     <br/>
     <input placeholder='Escribe una frase' onChange={(e)=>{setInputUser(e.target.value)}}></input>
     <button onClick={()=>{changeLetter()}}>Cambia la última letra a mayúsculas</button>
