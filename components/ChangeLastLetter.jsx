aa9dea8a09f5a406a77e71e637059fdbf1e8a029
Paula Cano <paulacanoroman@gmail.com>
2023-11-17 13:32:30 +0100

    funcionalidad cambiar letra final a mayúscula
    clonar y separar palabras
    recorrer array y separar por letras, coger ultima letra y pasar a mayúscula.
    coger las palabras sin la ultima letra e igualarlas a las letras mayúsculas
    retornar todas las letras unidas

--- components/ChangeLastLetter.jsx
+++ components/ChangeLastLetter.jsx
@@ -3,12 +3,27 @@ import React, { useState } from 'react'
 const ChangeLastLetter = () => {
 
     const [inputUser, setInputUser] = useState()
+
+    const changeLetter = ()=>{
+        const words = structuredClone(inputUser.split(" "))
+        const change = words.map((oneWord)=>{
+
+            const letterByLetter = oneWord.split("")
+            const upperLetters = letterByLetter.at(-1).toUpperCase()
+            letterByLetter[letterByLetter.length-1] = upperLetters
+            return(
+                letterByLetter.join("")
+            )
+        })
+        console.log(change)
+    
+    }
   return (
     <>
     {inputUser}
     <br/>
     <input placeholder='Escribe una frase' onChange={(e)=>{setInputUser(e.target.value)}}></input>
-    <button>Cambia la última letra a mayúsculas</button>
+    <button onClick={()=>{changeLetter()}}>Cambia la última letra a mayúsculas</button>
     </>
   )
 }
